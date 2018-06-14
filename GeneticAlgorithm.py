adcbfecd895fa92764e22e870048ffa496df242b
Dom Dias <domingos.adj@gmail.com>
2018-06-14 00:21:59 -0300

    * Removido alguns bugs

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -5,12 +5,12 @@ import random
 import math
 
 
-SURVIVAL_RATE = 0.3
+SURVIVAL_RATE = 0.2
 CROSSOVER_RATE = (1 - SURVIVAL_RATE)
 MUTATION_RATE = 0.01
 
 GENERATIONS_NUMBER = 3
-POPULATION_NUMBER = 6
+POPULATION_NUMBER = 8
 
 BINARY_SIZE = 2
 GENOTYPES_NUMBER = 3
@@ -21,7 +21,7 @@ def dec2bin(decimal):
     x = bin(decimal)
     x = x.split("b")[1]
 
-    for i in range(BINARY_SIZE - x.__len__()):
+    for index in range(BINARY_SIZE - x.__len__()):
         x = '0' + x
 
     return x
@@ -76,7 +76,7 @@ def goal(x, y, z):
 
 
 def operation(x, y, z):
-    return x + y + z
+    return int(x) + int(y) + int(z)
 
 
 def fitness(chromossome):
@@ -84,21 +84,20 @@ def fitness(chromossome):
     x = bin2dec(x)
     y = bin2dec(y)
     z = bin2dec(z)
-    print("Cromossomo: "+str(chromossome))
-    print("X Y Z {0}, {1}, {2}".format(x,y,z))
+    #print("Cromossomo: "+str(chromossome))
+
     goal_value = 6
 
     op = operation(x, y, z)
+    #print("X ({0}) + Y({1}) + Z({2})  = {3}".format(x,y,z,op))
+
+    if op > goal_value:
+        return 1
 
     distance = goal_value - op
-
-    if distance < 0:
-        distance = distance * -1
-
     fitness_value = 10 - distance
 
-    if fitness_value < 0:
-        return 1
+
 
     return fitness_value
 
@@ -119,21 +118,22 @@ def greater_fitness(a):
 
 def get_survivors(population):
 
-    number_individuals = math.ceil(POPULATION_NUMBER * SURVIVAL_RATE)
+    number_individuals = round(POPULATION_NUMBER * SURVIVAL_RATE)
 
     # ordena os valores da populacao de maneira decrescente pelo valor do fitness para a seleção
     population = sorted(population, key=greater_fitness)
 
     survivors = []
 
-    for i in range(number_individuals-1):
+    for i in range(number_individuals):
         survivors.append(population[len(population)-i-1])
 
     return survivors
 
 
 def roulette_wheel(population):
-    number_individuals = math.ceil(POPULATION_NUMBER * CROSSOVER_RATE)
+
+    number_individuals = round(POPULATION_NUMBER * CROSSOVER_RATE)
 
     total_sum = 0
 
@@ -141,21 +141,21 @@ def roulette_wheel(population):
     population = sorted(population, key=greater_fitness)
 
     # calcula o somatório do fitness de todos os individuos
-    for index in range(len(population) - 1):
+    for index in range(len(population)):
         total_sum += fitness(population[index])
 
     individuals = []
     selected_individuals_indexes = []
 
     # seleciona o número de individuos que irão fazer o crossover
-    for j in range(number_individuals - 1):
+    for j in range(number_individuals):
         # sorteia um número aleatório entre 0 e o somatório do fitness
-        print("Sum: "+str(total_sum))
+
         random_number = random.randint(0, total_sum)
 
         fitness_acum = 0
 
-        for index in range(len(population) - 1):
+        for index in range(len(population)):
             fitness_acum += fitness(population[index])
 
             # Indivíduo selecionado é o primeiro com aptidão acumulada maior que o número aleatório gerado
@@ -173,32 +173,21 @@ def one_point_crossover(parent_1, parent_2):
     cut_point = random.randint(0, length)
 
     son_1 = str(parent_1[0:cut_point]) + str(parent_2[cut_point: length])
-    son_2 = str(parent_2[0:cut_point]) + str(parent_1[cut_point: length])
 
-    return son_1, son_2
+    return son_1
 
 
 def crossover(population):
-    length = len(population)
     sons = []
 
-    for i in range(int(length/2)):
-        index_1 = random.randint(0, length-1)
-        index_2 = random.randint(0, length-1)
+    random.shuffle(population)
 
-        son_1, son_2 = one_point_crossover(population[index_1], population[index_2])
+    for index in range(len(population)):
 
-        sons.append(son_1)
-        sons.append(son_2)
+        i_1 = random.randint(0, len(population) - 1)
+        i_2 = random.randint(0, len(population) - 1)
 
-    #caso seja impar o número de filhos
-    if length > len(sons):
-        index_1 = random.randint(0, length - 1)
-        index_2 = random.randint(0, length - 1)
-
-        son_1 = one_point_crossover(population[index_1], population[index_2])
-
-        sons.append(son_1)
+        sons.append(one_point_crossover(population[i_1], population[i_2]))
 
     return sons
 
@@ -228,32 +217,33 @@ for i in range(GENERATIONS_NUMBER):
     print("* * * GERAÇÃO {0} * * *".format(i+1))
     # selecao dos individuos para a reproducao
     selected_individuals = roulette_wheel(new_population)
-    #print("Individuos selecionados...")
-    #print(selected_individuals)
-    #prinIdvs(selected_individuals)
+    print("Individuos selecionados...")
+    print(selected_individuals)
+    prinIdvs(selected_individuals)
     # realiza o cruzamento
     n_sons = crossover(selected_individuals)
-    #print("Filhos do cruzamento...")
-    #print(sons)
-    #prinIdvs(sons)
+    print("Filhos do cruzamento...")
+    print(n_sons)
+    prinIdvs(n_sons)
     # chance de mutação
     n_sons = mutation(n_sons)
-    #print("Mutação?...")
-    #print(sons)
+    print("Mutação?...")
+    print(n_sons)
     #pega os sobreviventes
     survivavors = get_survivors(new_population)
-    #print("Sobreviventes...")
-    #print(survivavors)
-    #prinIdvs(survivavors)
+    print("Sobreviventes...")
+    print(survivavors)
+    prinIdvs(survivavors)
 
     #nova população
-    new_population = survivavors + n_sons
+    new_population = (survivavors + n_sons)
 
 
 
 print("Nova população: ")
 print(new_population)
-prinIdvs(get_survivors(new_population))
+prinIdvs(new_population)
+
 print("Fim")
 
 
